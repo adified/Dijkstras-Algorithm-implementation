2b049e978a574c484fea8427b914a4e89c41ae2f
adified <105724242+adified@users.noreply.github.com>
2023-10-09 09:49:46 +0530

    Update dijkstra.cpp

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -43,7 +43,6 @@ public:
 
 				if(distTillNow + currentEdge < dist[nbr]){
 					//remove if nbr already exist in the set
-
 					auto f = s.find({dist[nbr],nbr});
 					if(f!=s.end()){
 						s.erase(f);
@@ -63,7 +62,6 @@ public:
 };
 
 int main(){
-
 	Graph g(5);
 	g.addEdge(0,1,1);
 	g.addEdge(1,2,1);
@@ -71,6 +69,5 @@ int main(){
 	g.addEdge(0,3,7);
 	g.addEdge(3,2,2);
 	g.addEdge(3,4,3);
-
 	cout << g.dijkstra(0,4)<<endl;
 }
