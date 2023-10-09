bb56333b83c0ed50df332b01f9a3604c70bd6815
adified <105724242+adified@users.noreply.github.com>
2023-10-09 09:47:47 +0530

    Update dijkstra.cpp

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -6,24 +6,19 @@
 using namespace std;
 
 class Graph{
-
 	int V;
 	list<pair<int,int> > *l;
-
 public:
 	Graph(int v){
 		V = v;
 		l = new list<pair<int,int> >[V];
 	}
-
 	void addEdge(int u,int v,int wt,bool undir = true){
-
 		l[u].push_back({wt,v});
 		if(undir){
 			l[v].push_back({wt,u});
 		}
 	}
-
 	int dijkstra(int src,int dest){
 
 		//Data Structures
@@ -35,7 +30,6 @@ public:
 		s.insert({0,src});
 
 		while(!s.empty()){
-
 			auto it = s.begin();
 			int node = it->second;
 			int distTillNow = it->first; 
@@ -44,7 +38,6 @@ public:
 			//Iterate over the nbrs of node
 			for(auto nbrPair : l[node]){
 				//......
-
 				int nbr = nbrPair.second;
 				int currentEdge = nbrPair.first;
 
@@ -58,20 +51,15 @@ public:
 					//insert the updated value with the new dist
 					dist[nbr] = distTillNow + currentEdge;
 					s.insert({dist[nbr],nbr});
-
 				}
-
 			}
-
 		}
 		//Single Source Shortest Dist to all other nodes
 		for(int i=0;i<V;i++){
 			cout<<"Node i "<<i <<" Dist "<<dist[i] <<endl;
 		}
 		return dist[dest];
-
 	}
-
 };
 
 int main(){
